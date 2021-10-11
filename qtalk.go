537841dc24645608f84f6a762306e013008279cc
Jeff Lindsay <progrium@gmail.com>
2021-10-11 16:52:58 -0500

    top level package that depends on subpackages

---
+++ qtalk.go
@@ -0,0 +1,9 @@
+package qtalk
+
+import (
+	_ "github.com/progrium/qtalk-go/codec"
+	_ "github.com/progrium/qtalk-go/fn"
+	_ "github.com/progrium/qtalk-go/mux"
+	_ "github.com/progrium/qtalk-go/rpc"
+	_ "github.com/progrium/qtalk-go/talk"
+)
