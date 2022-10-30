f6c435d22bb178431e98fc593bf384a55beef465
Jeff Lindsay <progrium@gmail.com>
2022-10-30 16:22:03 -0400

    fn: identify location of panic

--- fn/handler.go
+++ fn/handler.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"runtime"
+	"strings"
 
 	"github.com/mitchellh/mapstructure"
 	"github.com/progrium/qtalk-go/rpc"
@@ -63,7 +65,7 @@ func fromFunc(fn_ interface{}, rcvr_ interface{}) rpc.Handler {
 	return rpc.HandlerFunc(func(r rpc.Responder, c *rpc.Call) {
 		defer func() {
 			if p := recover(); p != nil {
-				r.Return(fmt.Errorf("panic: %s", p))
+				r.Return(fmt.Errorf("panic: %s [%s]", p, identifyPanic()))
 			}
 		}()
 
@@ -193,4 +195,32 @@ func ensureType(v reflect.Value, t reflect.Type) reflect.Value {
 		nv = nv.Convert(t)
 	}
 	return nv
+}
+
+func identifyPanic() string {
+	var name, file string
+	var line int
+	var pc [16]uintptr
+
+	n := runtime.Callers(3, pc[:])
+	for _, pc := range pc[:n] {
+		fn := runtime.FuncForPC(pc)
+		if fn == nil {
+			continue
+		}
+		file, line = fn.FileLine(pc)
+		name = fn.Name()
+		if !strings.HasPrefix(name, "runtime.") {
+			break
+		}
+	}
+
+	switch {
+	case name != "":
+		return fmt.Sprintf("%v:%v", name, line)
+	case file != "":
+		return fmt.Sprintf("%v:%v", file, line)
+	}
+
+	return fmt.Sprintf("pc:%x", pc)
 }
