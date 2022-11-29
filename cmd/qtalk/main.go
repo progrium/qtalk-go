2122ad37c251ee10f832174dc952676d7e99940f
Jeff Lindsay <progrium@gmail.com>
2022-11-28 20:38:39 -0500

    cmd: add missing close

--- cmd/qtalk/main.go
+++ cmd/qtalk/main.go
@@ -17,10 +17,12 @@ func main() {
 	flag.Parse()
 
 	cmd := flag.Arg(0)
-	if cmd != "call" {
-		log.Fatal("unknown command")
-		return
-	}
+	// if cmd != "call" {
+	// 	log.Fatal("unknown command")
+	// 	return
+	// }
+	fmt.Println(cmd)
+	return
 
 	u, err := url.Parse(flag.Arg(1))
 	if err != nil {
@@ -39,6 +41,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer peer.Close()
 
 	var ret any
 	_, err = peer.Call(context.Background(), u.Path, args, &ret)
